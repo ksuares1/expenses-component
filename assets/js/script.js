c3f45bea1f8cd7cd55f5789142905bb7369c4474
ksuares1 <kimberlyjsuares@gmail.com>
2023-03-10 15:04:27 -0500

    started adding js

---
+++ assets/js/script.js
@@ -0,0 +1,22 @@
+let itemizedBill= document.querySelector(".bill");
+
+let people= document.querySelector(".num-people");
+
+let fiveButton= document.querySelecto(".five-percent");
+
+let tenButton= document.querySelector(".ten-percent");
+
+let fifteenButton = document.querySelector(".fifteen-percent");
+
+let twentyFiveButton = document.querySelector(".twenty-five-percent");
+
+let fiftyPercentButton = document.querySelector(".fifty-percent");
+
+let customButton = document.querySelector(".custom");
+
+
+function calculationsApp(){
+
+}
+
+calculationsApp();
